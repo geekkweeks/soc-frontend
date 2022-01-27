5a9d288aa0c24cfced4277385f7d84a3dd19762d
geekkweeks <zahgreat30@gmail.com>
2022-01-27 15:30:46 +0700

    helper for getting fulldate

--- helpers/index.js
+++ helpers/index.js
@@ -1,5 +1,14 @@
-import cookie from 'cookie'
+import cookie from "cookie";
 
-export function parseCookies(req){
-    return cookie.parse(req ? req.headers.cookie || '' : '')
+export function parseCookies(req) {
+  return cookie.parse(req ? req.headers.cookie || "" : "");
+}
+
+export function getFullDate(req) {
+  if (!req) return null;
+  const takenDateSelected = new Date(req);
+  const dd = String(takenDateSelected.getDate()).padStart(2, "0");
+  const mm = String(takenDateSelected.getMonth() + 1).padStart(2, "0");
+  const yyyy = takenDateSelected.getFullYear();
+  return `${yyyy}-${mm}-${dd}`;
 }
